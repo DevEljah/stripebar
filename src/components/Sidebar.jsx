bff7dbfa9c18f665d2d38c91cd6dbb8b54421551
Jahid Eljah <jahideljah@gmail.com>
2023-02-05 19:42:05 +0100

    sidebar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,6 +16,26 @@ export default function Sidebar() {
         <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
+        <div className="sidebar-links">
+          {sublinks.map((sublink, index) => {
+            const { links, page } = sublink;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link;
+                    return (
+                      <a key={index} href={url}>
+                        {icon} {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
